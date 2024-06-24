c188df65fc5a23ce184a6136f75febc9756e27a3
romanov-enkrypt <23r.osborne@ldeutc.co.uk>
2024-06-24 10:59:44 +0100

    Reformatting & Sidebar Fix :D

---
+++ JS/footerheaderscript.js
@@ -0,0 +1,27 @@
+let toggle = false;
+function goX() {
+    location.assign("https://x.com")
+}
+function goInsta() {
+    location.assign("https://instagram.com")
+}
+function goYT() {
+    location.assign("https://youtube.com")
+}
+function goTiktok() {
+    location.assign("https://tiktok.com")
+}
+function revealSide() {
+    if (toggle) {
+        $("#sidebar").animate({left: '-20%'});
+    } else {
+        $("#sidebar").animate({left: '0%'});
+    }
+    toggle = !toggle;
+}
+function preOrderPage() {
+        $("#sidebar").animate({left: '-20%'});
+}
+$("#selectorbutton").click(function(){
+    $("#side").animate({left: '250px'});
+  }); 
